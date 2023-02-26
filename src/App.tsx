d2ed03d9da1d9ec982a89db05e60d97c65213a2b
Talwinder Singh <singhtalwinder790@gmail.com>
2023-02-26 17:34:14 +0530

    feat: add mock server

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Box } from '@mui/material';
 import TodoForm from './TodoForm';
+import createMockServer from './mock-server';
+
+createMockServer();
 
 function App() {
   return <Box sx={{ maxWidth: 500, margin: 'auto' }}><TodoForm /></Box>;
---
+++ src/mock-server.ts
@@ -0,0 +1,25 @@
+import { createServer, Response } from 'miragejs';
+
+function createMockServer({ environment = 'development' } = {}) {
+  let server = createServer({
+    environment,
+
+    routes() {
+      this.namespace = 'api'; // Define a namespace for all mock API routes
+
+      this.post('/todos', (schema, request) => {
+        const attrs = JSON.parse(request.requestBody);
+
+        if (attrs.title === 'Error') {
+          return new Response(400, {}, { errors: { title: 'Title cannot be "Error"' } });
+        }
+
+        return new Response(200, {}, { todo: attrs });
+      });
+    },
+  });
+
+  return server;
+}
+
+export default createMockServer;
